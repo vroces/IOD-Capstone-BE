401128df12a404e8719068a9ec3a1566a1b2ce3a
vroces <roces510@icloud.com>
2025-03-05 23:14:36 -0500

    Add initial project files and configuration

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const scheduleRoutes = require("./routes/scheduleRoutes");
+const userRoutes = require("./routes/userRoutes"); // User routes
+const playerCardRoutes = require("./routes/playerCardRoutes");  // Player card-related routes
+const followRoutes = require("./routes/followRoutes");  // Follow-related routes
+
+
+const app = express();
+
+// Middleware
+app.use(cors());  // Enable CORS for React frontend
+app.use(express.json());  // Parse incoming JSON requests
+app.use(express.static('public')); // Serve static files from 'public' folder
+
+const PORT = 5002;
+
+// Connect to MongoDB (for user data)
+mongoose.connect("mongodb://localhost:27017/sheplaysfootball")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+// Favicon route (avoids warning)
+app.get("/favicon.ico", (req, res) => res.status(204));
+
+// Use the user routes
+app.use("/api/", userRoutes);  // Add user-related routes
+
+// Use schedule routes
+app.use("/api/", scheduleRoutes);  // Add schedule routes
+
+// Use follow routes
+app.use("/api/follow", followRoutes);
+
+// Use player card routes
+app.use("/api/player-card", playerCardRoutes);
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));

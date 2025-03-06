4e30b586fe78f1bad91105e2909e83fd536410fb
vroces <roces510@icloud.com>
2025-03-06 11:34:31 -0500

    added URL comment routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,23 +5,23 @@ const router = express.Router();
 
 // Register route
 router.post("/register", registerUser);
-// http://localhost:5001/api/register
+// http://localhost:5002/api/register
 
 // Login route
 router.post("/login", loginUser);
-// http://localhost:5001/api/login
+// http://localhost:5002/api/login
 
 // Update user route
 router.put("/update/:userId", updateUser);
-// http://localhost:5001/api/update/{userID}
+// http://localhost:5002/api/update/{userID}
 
 // Get user by ID route
 router.get("/user/:userId", getUserById);
-// GET http://localhost:5001/api/user/{userID}
+// GET http://localhost:5002/api/user/{userID}
 
 // Get all users route
 router.get("/users", getAllUsers);
-// GET http://localhost:5001/api/users
+// GET http://localhost:5002/api/users
 
 
 module.exports = router;
